cda3dadb5bcb6829062fbed9e07db6c11173755f
Gabriel Oliveira <g.webdevelopr@gmail.com>
2020-06-01 19:46:25 -0300

    typescript support

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: node src/index.js
+web: npm run build && node build/index.js
 
---
+++ build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var app = express_1.default();
+var port = process.env.PORT || 8000;
+app.use(cors_1.default());
+app.get("/", function (req, res) {
+    res.send("Hello world 2");
+});
+app.listen(port, function () { return console.log("App running on port " + port); });
