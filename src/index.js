e474d21c383bb51e02139d8c7b4327a3662c6352
Gabriel Oliveira <g.webdevelopr@gmail.com>
2020-06-01 12:27:15 -0300

    there we go

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const cors = require("cors");
+
+const port = process.env.PORT || 8000
+
+
+app.use(cors)
+app.get("/", (req, res) => {
+	res.send("Hello world")
+
+})
+
+app.listen(port, () => console.log(`App running on port ${port}`))
+
+
